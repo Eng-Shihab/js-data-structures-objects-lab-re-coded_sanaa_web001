868dc58fd8bef750fd9fde3bd7c583789d623876
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-08-28 20:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ function destructivelyDeleteFromDriverByKey(obj, key) {
     delete obj['key'];
 }
 
+let driver={firstName:"driver1",lasttName:"ku"}
+
+function updateDriverWithKeyAndValue(obj,key,val){
+  return Object.assign({},obj,{ [key]: val })
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj,key,val){
+   return Object.assign(obj,{ [key]: val })
+}
+function deleteFromDriverByKey(obj,key){
+ const newObj = Object.assign({}, obj);
+ delete newObj[key];
+ return newObj;
+}
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
+}
